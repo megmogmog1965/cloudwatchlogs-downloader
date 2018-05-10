a6a97912a45eb97feddaae9abe84c411949d2467
Yusuke Kawatsu <meg.mog.mog1965@gmail.com>
2018-05-10 23:38:25 +0900

    No implicit production.

--- webpack.production.js
+++ webpack.production.js
@@ -4,9 +4,11 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 var main = merge(common.main, {
+  mode: 'production'
 })
 
 var renderer = merge(common.renderer, {
+  mode: 'production'
 })
 
 module.exports = [main, renderer];
