0acd834402d67763b99b4f3ef92e1a0f51d71631
Yusuke Kawatsu <meg.mog.mog1965@gmail.com>
2018-06-22 23:46:58 +0900

    remove duplicated test cases.

--- src/renderer/components/Settings.test.tsx
+++ src/renderer/components/Settings.test.tsx
@@ -13,25 +13,7 @@ describe('components/Settings [Cannot test it because it uses "redux-form"]', ()
       handleSubmit: mockHandleSubmit,
     };
 
+    // cannot test Filters due to redux-form.
     shallow(<Settings {...props} />);
-    // let wrapper = shallow(<Settings {...props} />);
-    // expect(wrapper.find('input').exists()).toBe(true);
-    // expect(wrapper.find('select[name="region"]').exists()).toBe(true);
-    // expect(wrapper.find('select[name="region"]').text()).toBe('ap-northeast-1');
-  });
-
-  it('called handleSubmit on click submit button.', () => {
-    let mockHandleSubmit = jest.fn();
-
-    let props = {
-      handleSubmit: mockHandleSubmit,
-    };
-
-    shallow(<Settings {...props} />);
-    // let wrapper = shallow(<Settings {...props} />);
-    // expect(wrapper.find('form.Settings').exists()).toBe(true);
-    //
-    // wrapper.find('form button').simulate('click'); // simulate form submit button clicked.
-    // expect(mockHandleSubmit.mock.calls.length).toBe({});
   });
 });
