ac2524a8f39d5b18bab8f4577842c04b82a4b44c
Yusuke Kawatsu <meg.mog.mog1965@gmail.com>
2018-06-24 20:15:57 +0900

    Add up/down button & css.

--- src/renderer/components/Filters.tsx
+++ src/renderer/components/Filters.tsx
@@ -43,38 +43,62 @@ const FilterList: React.SFC<any> = ({ fields, meta: { error, submitFailed } }) =
     <div className="form-group">
       <label><strong>Filters</strong></label>
 
-      <ul>
+      <ul className="list-group">
         {/* force to call componentDidUpdate */}
         <UpdatedHook />
 
-        <li>
-          <button type="button" className="btn btn-form btn-default" onClick={() => fields.push({})}>Add filter</button>
+        <li className="list-group-header">
+          <button type="button" className="btn btn-form btn-default" onClick={() => fields.push({})}>
+            <span className="icon icon-plus-squared icon-text" />Add filter
+          </button>
           {submitFailed && error && <span>{error}</span>}
         </li>
 
         {(fields as any[]).map((member, index) => (
-          <li key={index}>
-            <div className="form-group">
-              <Field id={`${member}.type`} name={`${member}.type`} component="select" className="form-control filter-type" onChange={onChange(`${member}`)}>
-                <option value="">None</option>
-                <option value="REPLACE_REGEX">Replace with regular expression</option>
-                <option value="EXTRACT_JSON">Extract a json value for the key</option>
-              </Field>
-            </div>
+          <li key={index} className="list-group-item">
+            <div className="media-body">
+              <div className="form-group">
+                <h4 className="pull-left">#{index}</h4>
+                <div className="pull-right btn-group">
+                  <button className="btn btn-mini btn-default" type="button" onClick={() => fields.swap(index, index - 1)}>
+                    <span className="icon icon-up-open" />
+                  </button>
+                  <button className="btn btn-mini btn-default" type="button" onClick={() => fields.swap(index, index + 1)}>
+                    <span className="icon icon-down-open" />
+                  </button>
+                  <button className="btn btn-mini btn-default" type="button" onClick={() => fields.remove(index)}>
+                    <span className="icon icon-cancel-squared" />
+                  </button>
+                </div>
+              </div>
 
-            <div id={`${member}.TransformReplaceRegex`} className="form-group">
-              <label>Regular expression pattern.</label>
-              <Field name={`${member}.pattern`} component="input" type="text" className="form-control" placeholder="Enter regular expression. (NOT partial match)" />
-              <label>Replacement.</label>
-              <Field name={`${member}.replacement`} component="input" type="text" className="form-control" placeholder="Enter replacement string. $1, $2... represent captured groups." />
-            </div>
+              <div className="form-group">
+                <Field id={`${member}.type`} name={`${member}.type`} component="select" className="form-control filter-type" onChange={onChange(`${member}`)}>
+                  <option value="">None</option>
+                  <option value="REPLACE_REGEX">Replace with regular expression</option>
+                  <option value="EXTRACT_JSON">Extract a json value for the key</option>
+                </Field>
+              </div>
 
-            <div id={`${member}.TransformExtractJson`} className="form-group">
-              <label>A root key of the json object.</label>
-              <Field name={`${member}.key`} component="input" type="text" className="form-control" placeholder={'e.g., Enter key \'log\' for the json formatted log \'{ "log": "..."}\' .'} />
-            </div>
+              <div id={`${member}.TransformReplaceRegex`}>
+                <div className="form-group">
+                  <label>Regular expression pattern.</label>
+                  <Field name={`${member}.pattern`} component="input" type="text" className="form-control" placeholder="Enter regular expression. (NOT partial match)" />
+                </div>
 
-            <button type="button" className="btn btn-form btn-negative" onClick={() => fields.remove(index)}>Remove</button>
+                <div className="form-group">
+                  <label>Replacement.</label>
+                  <Field name={`${member}.replacement`} component="input" type="text" className="form-control" placeholder="Enter replacement string. $1, $2... represent captured groups." />
+                </div>
+              </div>
+
+              <div id={`${member}.TransformExtractJson`}>
+                <div className="form-group">
+                  <label>A root key of the json object.</label>
+                  <Field name={`${member}.key`} component="input" type="text" className="form-control" placeholder={'e.g., Enter key \'log\' for the json formatted log \'{ "log": "..."}\' .'} />
+                </div>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
