b8418a4372807f3a30f446470b03975e34bdf443
Yusuke Kawatsu <meg.mog.mog1965@gmail.com>
2018-07-21 00:19:59 +0900

    prevent to package unnecessary node_modules.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp');
+var jeditor = require("gulp-json-editor");
 
 gulp.task('copy', function() {
   gulp.src(['public/**/*'])
+    .pipe(gulp.dest('dist'));
+
+  gulp.src('package.json')
+    .pipe(jeditor(function(json) {
+      json.devDependencies = {};
+      json.dependencies = {};
+      return json;
+    }))
     .pipe(gulp.dest('dist'));
 });
